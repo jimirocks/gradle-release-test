82ff10a681e41c3d13143e0cd9556cc288ffd740
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2020-09-02 22:02:49 +0200

    Try to fix usage of researchgate

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,14 @@
-import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
 
 plugins {
     kotlin("jvm") version "1.3.72"
 
     // releasing
-//    id("net.researchgate.release") version "2.8.1"
-    id("pl.allegro.tech.build.axion-release") version "1.12.0"
-
-
+    id("net.researchgate.release") version "2.8.1"
 }
-
-scmVersion {
-    tag(closureOf<TagNameSerializationConfig> {
-        prefix = project.name
-    })
-}
-
-project.version = scmVersion.version
 
 allprojects {
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
-//    apply(plugin = "net.researchgate.release")
 
 
     repositories {
