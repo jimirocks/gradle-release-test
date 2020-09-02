9d7e717f2380c94de59fd868cd3ba055f8d8a32b
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2020-09-02 21:15:53 +0200

    Multimodule test with axion

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,19 @@ plugins {
     kotlin("jvm") version "1.3.72"
 
     // releasing
-    id("net.researchgate.release") version "2.8.1"
+//    id("net.researchgate.release") version "2.8.1"
+    id("pl.allegro.tech.build.axion-release") version "1.12.0"
+
+
 }
 
 allprojects {
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "net.researchgate.release")
+//    apply(plugin = "net.researchgate.release")
+    apply(plugin = "pl.allegro.tech.build.axion-release")
+
+    project.version = scmVersion.version
 
     repositories {
         mavenLocal()
